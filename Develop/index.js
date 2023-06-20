474caf76f4656a51259d5f3582fa413647efbc38
Black Birdie <dannonrogers@gmail.com>
2023-06-20 08:11:13 -0700

    Code cleanup

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,6 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -109,19 +107,3 @@ ${tests}
         err ? console.error(err) : console.log('Successfully created README.md!')
       );
     });
-
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile('README.md', markdownPageContent, (err) =>
-//         err ? console.error(err) : console.log('Successfully created new file!')
-//     );
-// }
-
-// // TODO: Create a function to initialize app
-// function init() {
-//     writeToFile();
-// }
-
-// // Function call to initialize app
-// init();
